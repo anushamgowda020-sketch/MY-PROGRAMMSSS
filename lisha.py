0eb7b3cdd8b96e49b4094e5af8a8015f72ba8771
Lisha M Gowda <anushamgowda020@gmail.com>
2026-03-13 20:37:18 +0530

    Update lisha.py

--- lisha.py
+++ lisha.py
@@ -1,5 +1,11 @@
+#day 1
 print("hello users")
+#day2
 a=int(input("Enter the value of a"))
 b=int(input("Enter the value of b"))
 c=a+b
 print("The sum of two numbers is:",c)
+#day 3
+rad=float(input("Enter the radius of circle="))
+Area=3.14*r**2
+print("Area of circle =",Area)
