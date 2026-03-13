71e677e24e62ff97e47a134cd342aba24e998d9b
Lisha M Gowda <anushamgowda020@gmail.com>
2026-03-13 20:52:35 +0530

    Update lisha.py

--- lisha.py
+++ lisha.py
@@ -5,7 +5,20 @@ a=int(input("Enter the value of a"))
 b=int(input("Enter the value of b"))
 c=a+b
 print("The sum of two numbers is:",c)
-#day 3
+#day 3 - pro 1
 rad=float(input("Enter the radius of circle="))
 Area=3.14*r**2
 print("Area of circle =",Area)
+# day 3 - pro 2
+num1=float(input("Enter the first number for addtion:"))
+num2=float(input("Enter the second number for addtion:"))
+sum_result=num1+num2
+print(f"Sum:{num1}+{num2}={sum_result})
+#day 4 - pro 3
+num3=float(input("Enter the first number for division:"))
+num4=float(input("Enter the second number for division:"))
+if num4==0:
+print("Error: Division error ")
+else:
+div_result=num3/num4
+print("The quotient of the division =",div_result)
